0bb954178e2cb102464fc5c857dee7ff51118239
Jayedkhan800 <tanvirgaming859@gmail.com>
2025-01-11 21:26:59 +0600

    Update script.sh

--- script.sh
+++ script.sh
@@ -12,7 +12,7 @@ echo "Removing stuffs success..."
 echo "===================================="
 
 # Clone local_manifests repository
-git clone https://github.com/AsTechpro20/local_manifests_blossom.git -b Lineage-22 .repo/local_manifests
+git clone https://github.com/Jayedkhan800/local_manifests_blossom.git -b Misi-15 .repo/local_manifests
 echo "===================================="
 echo "Cloning local_manifests was success..."
 echo "===================================="
