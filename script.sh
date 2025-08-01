6f2dfd4623df78d991d75b354700e372d8c474ca
Jayed khan <tanvirgaming859@gmail.com>
2025-08-01 16:18:44 +0600

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,55 +1,8 @@
-#Lineage-22 build_script_blossom
-
-# Remove some stuffs
-rm -rf .repo/local_manifests/ 
-rm -rf  vendor/xiaomi
-rm -rf  kernel/xiaomi
-rm -rf  device/xiaomi
-rm -rf  device/mediatek
-rm -rf  hardware/mediatek
-echo "===================================="
-echo "Removing stuffs success..."
-echo "===================================="
-
-# Clone local_manifests repository
-git clone https://github.com/Jayedkhan800/local_manifests_blossom.git -b Alpha-15 .repo/local_manifests
-echo "===================================="
-echo "Cloning local_manifests was success..."
-echo "===================================="
-
-# Initialize Alpha-15 repo
-repo init -u https://github.com/alphadroid-project/manifest -b alpha-15.1-wip --git-lfs
-echo "===================================="
-echo "Initializing Alpha-15 repo was success..."
-echo "===================================="
-
-# Sync the repositories
-/opt/crave/resync.sh || repo sync
-echo "===================================="
-echo "Syncing the repositories was success..."
-echo "===================================="
-
-# Auto-sign build
-rm -rf vendor/lineage-priv/keys
-wget https://raw.githubusercontent.com/Trijal08/crDroid-build-signed-script-auto/main/create-signed-env.sh
-chmod a+x create-signed-env.sh
-./create-signed-env.sh
-
-# Exports
-echo "===================================="
-echo "Adding exports..."
-echo "===================================="
-export BUILD_USERNAME=Jayed Khan
-export BUILD_HOSTNAME=crave
-
-# Set up build environment
-echo "===================================="
-echo "Setting up build environment..."
-echo "===================================="
+repo init -u https://github.com/Evolution-X/manifest -b bka --git-lfs
+/opt/crave/resync.sh
+repo sync -c -j4 --force-sync --no-clone-bundle --no-tags
+git clone https://github.com/Jayedkhan800/local_manifests_blossom.git -b lineage-22.2 .repo/local_manifests
+repo sync
 source build/envsetup.sh
-
-#Building Lineage-20
-echo "===================================="
-echo "Building Alpha-15..."
-echo "===================================="
-source build/envsetup.sh || brunch blossom 
+lunch lineage_blossom-bp2a-userdebug
+m evolution
